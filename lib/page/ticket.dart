988b403f3b6f2695185be0a7969e81c1f98f42df
Juste231 <avademejustelelouch@gmail.com>
2024-12-17 17:01:57 +0100

    commit

---
+++ lib/page/ticket.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class ticket extends StatefulWidget {
+  const ticket({super.key});
+
+  @override
+  State<ticket> createState() => _ticketState();
+}
+
+class _ticketState extends State<ticket> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text("tdfyfydydyddgxtff"),
+        elevation: 0,
+        centerTitle: true,
+      ),
+    );
+  }
+}
